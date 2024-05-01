26ae50a5e5172b6f9085c67b479acc1f2f565a9f
Vitaliy-Igonin <132227313+Vitaliy-Igonin@users.noreply.github.com>
2024-05-01 10:11:38 +0400

    Add files via upload
    Создать страницу, на которой присутствуют поля для ввода логина и пароля, а также кнопка «Отправить». При нажатии на кнопку на серверной стороне должна производиться проверка введенных логина и пароля. Значения допустимых логинов и соответствующих им паролей должны храниться в типизированной коллекции Dictionary. Если введенные значения логина и пароля соответствуют одной паре, присутствующей в коллекции, должна открываться страница с надписью «Добро пожаловать!». Если введенные значения отсутствуют в коллекции, то нужно вывести соответствующее сообщение на странице логина, и предложить пользователю заново ввести логин и пароль.

---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Dictionary<string, string> dict = new Dictionary<string, string>
+            {
+                ["user"] = "admin",
+                ["user1"] = "admin1"
+            };
+
+            string inputLogin = textBox1.Text;
+            string inputPass = textBox2.Text;
+
+            if (dict.ContainsKey(inputLogin) && dict[inputLogin] == inputPass)
+            {
+                Form2 s = new Form2();
+                s.Show();
+
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Неправильный логин или пароль");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
